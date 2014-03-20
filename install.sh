0fe71dd361375eaf69de745e436bc02d9ba4a9e0
Miguel Palhas <mpalhas@gmail.com>
2014-03-20 13:41:58 +0000

    Renames dir and uncomments installation

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-files=`find . -mindepth 1 -maxdepth 1 ! -name install.sh ! -name no_copy`
+files=`find . -mindepth 1 -maxdepth 1 ! -name install.sh ! -name private ! -name dotfiles.sh`
 
 for file in $files; do
   filename=${file//\.\//}
@@ -9,9 +9,9 @@ for file in $files; do
 
   if [ -e $dst ]; then
     echo "backing up $filename"
-    # mv $dst "$HOME/old_$filename"
+    mv $dst "$HOME/old_$filename"
   fi
 
   echo "symlinking $filename"
-  # ln -s $src $dst
+  ln -s $src $dst
 done
