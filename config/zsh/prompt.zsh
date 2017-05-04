817bd4f0426125adc2e97f8bf0e6375c1f90baf4
Miguel Palhas <mpalhas@gmail.com>
2017-05-04 20:08:32 +0100

    Adds comments on how to profile all commands executed

--- config/zsh/prompt.zsh
+++ config/zsh/prompt.zsh
@@ -36,8 +36,17 @@ path () {
 export GIT_RADAR_COLOR_BRANCH="%{%B$fg[yellow]%}"
 export GIT_RADAR_FORMAT=" \x01\033[1;30m\x02\x01\033[0m\x02%{remote: }%{branch}%{ :local}\x01\033[1;30m\x02\x01\033[0m\x02%{ :changes}"
 
-
 PROMPT=$'\n'
 PROMPT=${PROMPT}'${blue}`path`'
 PROMPT=${PROMPT}'$(~/.config/git-radar/git-radar --zsh --fetch)'$'\n'
 PROMPT=${PROMPT}'${green}`prompt_char`${reset} '
+
+# TODO time spent on last command
+# preexec() {
+#   typeset -ig _start=SECONDS
+# }
+
+# precmd() {
+#   set -A _elapsed $(( SECONDS-_start ))
+# }
+# RPROMPT='%{%B$fg[yellow]%}`printf %.2g $((${_elapsed[-1]}))`s'
