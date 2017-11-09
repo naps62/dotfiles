014d3f45b5d090ae04fc33f8254c6302f8ddfb8b
Miguel Palhas <mpalhas@gmail.com>
2017-11-09 10:25:34 +0000

    Better support for sudo prompt

--- config/zsh/completions.zsh
+++ config/zsh/completions.zsh
@@ -4,7 +4,15 @@
 fpath=(~/.config/zsh/completions.d $fpath)
 
 autoload -U compinit
-compinit
+
+if [ $UID ]; then
+  # We are root (probably in sudo mode?), do not check for insecure directories
+  # since this check was done for your regular user, and since some files in
+  # fpath do not belong to root but to your regular user
+  compinit -u
+else
+  compinit
+fi
 
 # zstyle ':completion:*' menu select=2
 # zstyle ':completion:*:*:git:*' user-commands ${${(M)${(k)commands}:#git-*}/git-/}
--- config/zsh/prompt.zsh
+++ config/zsh/prompt.zsh
@@ -10,10 +10,21 @@ green="%{%B%F{green}%}"
 red="%{%B%F{red}%}"
 reset="%{$reset_color%}"
 
+root_warning () {
+  [[ "$USERNAME" ==  "root" ]] && echo "[root] "
+}
+
 prompt_char () {
   echo -n "%(?.${green}.${red})"
+
+  # if we are root, prompt is a #
+  [[ "$USERNAME" == "root" ]] && echo '#' && return
+
+  # if we are in a git repo, prompt is ±
   command git rev-parse --git-dir >/dev/null 2>/dev/null && echo '±' && return
-  [ $USERNAME != "root" ] && echo '$' || echo '#'
+
+  # otherwise, a good-ol $
+  echo '$'
 }
 
 git_path () {
@@ -22,7 +33,9 @@ git_path () {
 
   path_in_repo=${PWD#$repo_path}
   path_in_repo=${path_in_repo#/}
-  echo "$green$repo_name"' '$blue$path_in_repo
+  full="$green$repo_name"$blue' '$path_in_repo
+
+  echo ${full%% }
 }
 
 not_git_path () {
@@ -30,6 +43,7 @@ not_git_path () {
 }
 
 path () {
+  # echo "asd"
   ((git rev-parse 2>/dev/null >/dev/null) && git_path) || not_git_path
 }
 
@@ -37,7 +51,7 @@ export GIT_RADAR_COLOR_BRANCH="%{%B$fg[yellow]%}"
 export GIT_RADAR_FORMAT=" \x01\033[1;30m\x02\x01\033[0m\x02%{remote: }%{branch}%{ :local}\x01\033[1;30m\x02\x01\033[0m\x02%{ :changes}"
 
 PROMPT=$'\n'
-PROMPT=${PROMPT}'${blue}`path`'
+PROMPT=${PROMPT}'${red}`root_warning`${blue}`path`'
 PROMPT=${PROMPT}'$(~/.config/git-radar/git-radar --zsh --fetch)'$'\n'
 PROMPT=${PROMPT}'${green}`prompt_char`${reset} '
 
