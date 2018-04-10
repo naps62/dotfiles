3c5c7729e353a6215cc050cd625bb2815ef7325b
Miguel Palhas <mpalhas@gmail.com>
2018-04-10 23:47:35 +0100

    New server|

--- config/zsh/theme.zsh
+++ config/zsh/theme.zsh
@@ -2,11 +2,4 @@
 
 autoload -U colors && colors
 
-#
-# base16 shell base configuration
-#
-BASE16_SHELL=$HOME/.config/base16-shell/
-BASE16_SHELL_HOOKS=$HOME/.config/base16-hooks
-
-[ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"
-
+base16-manager set eighties > /dev/null
