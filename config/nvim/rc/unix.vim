36c584c58f19c1d1d8d8255c4d666ab4f06aec65
Miguel Palhas <mpalhas@gmail.com>
2020-06-14 23:56:03 +0100

    Several updates

--- config/nvim/rc/unix.vim
+++ config/nvim/rc/unix.vim
@@ -7,10 +7,10 @@ if has("unix")
   if s:uname == "Darwin\n"
     set mouse=nicr
 
-    let g:python_host_prog='/usr/bin/python'
+    let g:python_host_prog='/usr/bin/python2'
     let g:python3_host_prog='/usr/local/bin/python3'
   else
-    let g:python_host_prog='/usr/bin/python'
+    let g:python_host_prog='/usr/bin/python2'
     let g:python3_host_prog='/usr/bin/python3'
   endif
 end
--- config/zsh/prompt.zsh
+++ config/zsh/prompt.zsh
@@ -69,8 +69,8 @@ path () {
   ((git rev-parse 2>/dev/null >/dev/null) && git_path) || not_git_path
 }
 
-export GIT_RADAR_COLOR_BRANCH="%{%B$fg[yellow]%}"
-export GIT_RADAR_FORMAT=" \x01\033[1;30m\x02\x01\033[0m\x02%{remote: }%{branch}%{ :local}\x01\033[1;30m\x02\x01\033[0m\x02%{ :changes}"
+# export GIT_RADAR_COLOR_BRANCH="%{%B$fg[yellow]%}"
+# export GIT_RADAR_FORMAT=" \x01\033[1;30m\x02\x01\033[0m\x02%{remote: }%{branch}%{ :local}\x01\033[1;30m\x02\x01\033[0m\x02%{ :changes}"
 
 PROMPT=$'\n'
 PROMPT=${PROMPT}'`root_warning``remote_warning`${blue}`path`'
