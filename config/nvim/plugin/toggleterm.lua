8ffc7b0c20790ee5d066402df02a1d674bcc7865
Miguel Palhas <mpalhas@gmail.com>
2022-07-03 21:38:14 +0100

    adding lazygit

--- config/nvim/plugin/toggleterm.lua
+++ config/nvim/plugin/toggleterm.lua
@@ -1,1 +1,12 @@
 require('toggleterm').setup()
+
+local Terminal  = require('toggleterm.terminal').Terminal
+local lazygit = Terminal:new({ 
+  cmd = "lazygit",
+  direction = "float",
+  hidden = true
+})
+
+function _lazygit_toggle()
+  lazygit:toggle()
+end
