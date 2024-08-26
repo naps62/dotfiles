bb3eef1cbf1455f7aa58e9d3c3a57198ed76421b
Miguel Palhas <mpalhas@gmail.com>
2024-08-26 12:13:13 +0100

    code review

--- config/nvim/lua/plugins/ai.lua
+++ config/nvim/lua/plugins/ai.lua
@@ -1,14 +1,45 @@
 return {
-  {
-    "jackMort/ChatGPT.nvim",
-    event = "VeryLazy",
-    config = function()
-      require("chatgpt").setup()
-    end,
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim"
-    }
-  }
+	{
+		"jackMort/ChatGPT.nvim",
+		event = "VeryLazy",
+		config = function()
+			require("chatgpt").setup()
+		end,
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+	},
+
+	{
+		"olimorris/codecompanion.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-telescope/telescope.nvim", -- Optional
+			{
+				"stevearc/dressing.nvim", -- Optional: Improves the default Neovim UI
+				opts = {},
+			},
+		},
+		config = function()
+			require("codecompanion").setup({
+				strategies = {
+					chat = { adapter = "anthropic" },
+					inline = { adapter = "anthropic" },
+					agent = { adapter = "anthropic" },
+				},
+				adapters = {
+					anthropic = function()
+						return require("codecompanion.adapters").extend("anthropic", {
+							env = {
+								api_key = os.getenv("ANTHROPIC_API_KEY"),
+							},
+						})
+					end,
+				},
+			})
+		end,
+	},
 }
